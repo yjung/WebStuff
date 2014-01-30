247eb3353af2f7367718a8b98a017a99b0b010d6
Your Name Here <hanna.trunk@informatik.hs-fulda.de>
2014-01-30 12:10:23 +0100

    Katalogtraversierung implementiert
    Zugehörigen Bilder in Dropbox.

--- 3Dcatalogue/3Dcatalogue/main.js
+++ 3Dcatalogue/3Dcatalogue/main.js
@@ -1,5 +1,4 @@
-var jsonObj = null;
-
+var catalog = null;
 
 window.onload=function(evt){
     load();
@@ -9,11 +8,23 @@ function load()
 {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
-        jsonObj = eval('(' + this.responseText + ')');
-       addData(jsonObj);
+        catalog = eval('(' + this.responseText + ')');
+        initCatalog(catalog);
+       addData(catalog);
     };
     xhr.open("GET", "Catalog.json", true);   // async
     xhr.send();
+}
+
+function initCatalog(jsonObj){
+    for(var i=0; i< jsonObj.length; i++)
+    {
+        jsonObj[i].parent = jsonObj;
+        if(jsonObj[i].Entries && jsonObj[i].Entries.length)
+        {
+            initCatalog(jsonObj[i].Entries);
+        }
+    }
 }
 
 function addData(jsonObj){
@@ -22,9 +33,10 @@ function addData(jsonObj){
     {
         var tr = document.createElement("tr");
         var td1 = document.createElement("td");
-        var stri = "<img src=\"bilder/" + jsonObj[i].Image +"\"><br>" + jsonObj[i].Description;
+        var stri = "<img src=\"DEMO01_FurnitureShop/Catalog/" + jsonObj[i].Image +"\"><br>" + jsonObj[i].Description;
         td1.innerHTML = stri;
         td1.myEntries = jsonObj[i].Entries;
+
         if (td1.myEntries && td1.myEntries.length)
             td1.onclick = (function(pos) {
                 return function() {
@@ -44,8 +56,13 @@ function addParent(node) {
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
     console.log(node);
-    var stri = "<img src=\"bilder/" + node.Image +"\"><br>" + node.Description;
+    var stri = "<img src=\"DEMO01_FurnitureShop/Catalog/" + node.Image +"\"><br>" + node.Description;
     td1.innerHTML = stri;
+    td1.onclick = (function() {
+            table.innerHTML = "";
+
+            addData(node.parent);
+        });
     tr.appendChild(td1);
     table.appendChild(tr);
 }
