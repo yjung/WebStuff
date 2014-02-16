5dd6a6606208769b3ba9563982af472cfbaa2bc1
oliverkrah <oliverkrah@online.de>
2014-02-16 15:04:14 +0100

    Update auf aktuellen Stand
    Spielablauf steht. Question-JSON-Modell steht. Usw.

---
+++ MatheLernspiel_X3DOM/js/custom.js
@@ -0,0 +1,158 @@
+var questions;
+var x3dObjects;
+
+var activeBlock;
+var activeQuestion;
+
+$(document).ready(function() {
+	
+	$('#loadQuestions').click(function() {
+		
+		$.getJSON( "questions/questions2.js", function( data ) {
+		
+			questions = data;
+			loadQuestions();
+		
+		}).fail(function( jqxhr, textStatus, error ) {
+			alert( "Request Failed: " + textStatus + ", " + error );
+		});
+		
+	});
+	
+	$('#validateAnswer').click(function() {
+		if(checkAnswer()) {
+			$('#questionBoard').css({
+				'background': '#2ecc71',
+				'color': '#ffffff'
+			});
+			if(!loadNextQuestion())
+				finalize();
+		}
+		else {
+			$('#questionBoard').css({
+				'background': '#e74c3c',
+				'color': '#ffffff'
+			});
+		}
+	});
+	
+});
+
+function initInline() {
+	$('#test__theScene').find('Group').click(function() {
+
+		var objThis = $(this).attr('id');
+	
+		$('#test__theScene').find('Group').each(function() {
+			if($(this).attr('id') != objThis)
+				$(this).find('Appearance Material').attr('diffuseColor', 'gray');
+		});
+	
+		var obj = $(this).find('Appearance Material');
+	
+		if($(obj).attr('diffuseColor') == '#3498db') {
+			$(obj).attr('diffuseColor', 'gray');
+			$('#questionTips').empty();
+		} else {
+			$(obj).attr('diffuseColor', '#3498db');
+			showObjInfo($(this).attr('id'));
+		}
+	});
+}
+
+function loadQuestions() {
+	$('#questions ul').empty();
+	
+	var questionCount = 0;
+	$.each(questions, function(i, item) {
+		$.each(item.fragen, function(j, fragen) {
+			var summe = parseInt(i) + parseInt(j);
+			$('#questions ul').append('<li data-block="' + i + '" data-number="' + summe + '">' + fragen.frage + '<span></span></li>');
+			questionCount++;
+		});
+	});
+	
+	//update number of questions in "Fragen - Button"
+	$('#myheader a span').html('(' + questionCount + ')');
+	
+	//disable "Fragen laden"-Button
+	$('#loadQuestions').addClass('ui-disabled');
+	$('#validateAnswer').removeClass('ui-disabled');
+	
+	//Erste Frage laden
+	loadNextQuestion();
+}
+
+function loadNextQuestion() {
+	//active question number | next question number
+	var aqn = parseInt($('#questions li.active').attr('data-number'));
+	var nqn = aqn;
+	
+	//active block number
+	var abn = parseInt($('#questions li.active').attr('data-block'));
+	
+	if(!$('#questions li.active').length) {
+		nqn = 0;
+		abn = 1;
+	}
+	
+	//aktive Frage setzen
+	activeBlock = questions[abn];
+	activeQuestion = questions[abn].fragen[nqn];
+	
+	if(typeof activeQuestion == 'undefined')
+		return false;
+
+	var summe = parseInt(abn) + parseInt(nqn);
+	
+	//Fragebereich aktualisieren
+	$('#questionContent .question').html('<span>Frage ' + summe + ':</span> '+ activeQuestion.frage);
+	$('#questionContent .info span').html('Antwort in ' + activeQuestion.einheit);
+	$('#questionContent .formel span').html(activeQuestion.formel);
+	
+	//Sidebar aktualisieren
+	$('#questions li.active').removeClass('active');
+	$('#questions li[data-number="' + summe + '"]').addClass('active');
+	
+	//Model aktualisieren
+	$('#the3DContent inline').attr('url', 'models/' + activeBlock.model + '.x3d');
+	
+	//Clear Input
+	$('#questionContent input[name="answer"]').val('');
+	
+	//Clear Question Tip
+	$('#questionTips').empty();
+	
+	setTimeout(function(){
+		$('#questionBoard').css({
+			'background': '#bdc3c7',
+			'color': '#333'
+		});
+	}, 400);
+	
+	return true;
+}
+
+function showObjInfo(obj) {
+	$('#questionTips').empty();
+	$('#questionTips').append('<h4>' + obj + '</h4><table></table>');
+	
+	var groupDef = obj.replace(/test__/g, '');
+	
+	$.each(activeBlock.tips[groupDef], function(i, tip) {
+		$('#questionTips table').append('<tr><td>' + tip.name + '</td><td>' + tip.wert + '</td></tr>');
+	});
+}
+
+function checkAnswer() {
+	var userAnswer = $('#questionContent input[name="answer"]').val();
+	
+	if(userAnswer == activeQuestion.ergebnis)
+		return true;
+		
+	return false;
+}
+
+function finalize() {
+	$('body').append('<div id="finalizer"></div>');
+}
---
+++ MatheLernspiel_X3DOM/questions/questions2.js
@@ -0,0 +1,49 @@
+{
+	"1": {
+		"model": "testModel",
+		"fragen": {
+			"0": {
+				"frage": "Wie gro&szlig; ist der Umfang?",
+				"formel": "U = 2 * pi * r",
+				"ergebnis": "6.282",
+				"einheit": "cm&sup3;"
+			},
+			"1": {
+				"frage": "Wie gro&szlig; ist die Grundfl&auml;che?",
+				"formel": "G = r&sup2; * pi",
+				"ergebnis": "3.141",
+				"einheit": "cm&sup2;"
+			}
+		},
+		"tips" : {
+			"group_ME_Cone": {
+				"1" : {
+					"name": "Radius",
+					"wert": "r = 1cm"
+				},
+				"2" : {
+					"name": "H&ouml;he",
+					"wert": "h = 2cm"
+				},
+				"3" : {
+					"name": "PI",
+					"wert": "pi = 3.141"
+				}
+			},
+			"group_ME_Cylinder": {
+				"1" : {
+					"name": "Radius",
+					"wert": "r = 2cm"
+				},
+				"2" : {
+					"name": "H&ouml;he",
+					"wert": "h = 3cm"
+				},
+				"3" : {
+					"name": "PI",
+					"wert": "pi = 4.141"
+				}
+			}
+		}
+	}
+}
