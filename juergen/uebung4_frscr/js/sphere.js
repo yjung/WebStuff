21fcfcf80195d11c781b6d0511baf7bf5991d213
nineto <greatbelow@gmx.de>
2014-06-11 16:45:44 +0200

    uploading uebung4_frscr

--- juergen/uebung4_frscr/js/sphere.js
+++ juergen/uebung4_frscr/js/sphere.js
@@ -1,4 +1,4 @@
-var cube = {
+var sphere = {
 
     vertices: [
         // vordere Fläche
@@ -39,12 +39,12 @@ var cube = {
     ],
     // the object's vertex colors
     colors: [
-        1.0,  1.0,  1.0,  1.0,    // vordere Fläche: weiß
-        1.0,  0.0,  0.0,  1.0,    // hintere Fläche: rot
-        0.0,  1.0,  0.0,  1.0,    // obere Fläche: grün
-        0.0,  0.0,  1.0,  1.0,    // untere Fläche: blau
-        1.0,  1.0,  0.0,  1.0,    // rechte Fläche: gelb
-        1.0,  0.0,  1.0,  1.0     // linke Fläche: violett
+        [1.0,  1.0,  1.0,  1.0],    // vordere Fläche: weiß
+        [1.0,  0.0,  0.0,  1.0],    // hintere Fläche: rot
+        [0.0,  1.0,  0.0,  1.0],    // obere Fläche: grün
+        [0.0,  0.0,  1.0,  1.0],    // untere Fläche: blau
+        [1.0,  1.0,  0.0,  1.0],    // rechte Fläche: gelb
+        [1.0,  0.0,  1.0,  1.0]     // linke Fläche: violett
     ],
     // the object's texture coordinates
     texCoords: [
@@ -55,56 +55,39 @@ var cube = {
         0.5, 1,
         0,   0,
         1,   0,
-        0,   1,
-
-        0,   0,
-        1,   0,
-        0,   1,
-        1,   1,
-        0.5, 1,
-        0,   0,
-        1,   0,
-        0,   1,
-
-        0,   0,
-        1,   0,
-        0,   1,
-        1,   1,
-        0.5, 1,
-        0,   0,
-        1,   0,
         0,   1
+
     ],
     // index array for drawing a quad (consisting of two tris)
     indices: [
-     // vorne
-     // A  B  C
+        // vorne
+        // A  B  C
         0, 1, 2,
-     // B  D  C
+        // B  D  C
         1, 3, 2,
 
-     // oben
-     // C  D  E
+        // oben
+        // C  D  E
         2, 3, 4,
-     // D  F  E
+        // D  F  E
         3, 5, 4,
 
-     // links
-     // H  G  E
+        // links
+        // H  G  E
         7, 6, 4,
-     // E  F  H
+        // E  F  H
         4, 5, 7,
 
-     // rechts
-     // B  H  D
+        // rechts
+        // B  H  D
         1, 7, 3,
-     // H  F  D
+        // H  F  D
         7, 5, 3,
 
-     // hinten
-     // C  G  A
+        // hinten
+        // C  G  A
         2, 6, 0,
-     // C  E  G
+        // C  E  G
         2, 4, 6
 
     ],
@@ -122,6 +105,6 @@ var cube = {
     animating: false
 
     // texture
-   // imgSrc: "altedachziegeln_512.jpg",
-   // texture: null
+    // imgSrc: "altedachziegeln_512.jpg",
+    // texture: null
 };
